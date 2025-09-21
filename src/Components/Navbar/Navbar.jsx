cf3f567cb003214b0891a65bffeb6704956d1db5
AbdelkaderJIT <abdelkader.jerbi.it@gmail.com>
2025-09-21 22:40:59 +0100

    feature responsive

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,25 +1,126 @@
-import React from 'react'
-import './Navbar.css'
-import logo from '../Assets/logo_shopping.png'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import logo from '../Assets/logo_shopping.png';
+import { Link } from 'react-router-dom';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+import MenuItem from '@mui/material/MenuItem';
 
-const Navbar = () => {
+const pages = [
+  { label: 'Shop', path: '/' },
+  { label: 'Favourite products', path: '/favourites' }
+];
 
-    const [menu,setMenu] = React.useState("shop")
+
+function Navbar() {
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
 
   return (
-    <div className='navbar'>
-        <div className='nav-logo'>
-           <Link to="/"><img src={logo} alt='logo' /></Link>
-            <p>Product Catalog</p>
-        </div>
-        <ul className="nav-menu">
-            <li onClick={() => setMenu("shop")}><Link style={{ textDecoration: 'none'}} to="/">Shop</Link> {menu === "shop"?<hr />:<></>}</li>
-            <li onClick={() => setMenu("favourites")}><Link style={{ textDecoration: 'none'}} to="/favourites">Favourite products</Link> {menu === "favourites"?<hr />:<></>}</li>
-        </ul>
-        
-    </div>
-  )
+    <AppBar position="static" sx={{ background: '#fff', color: '#222' }}>
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          
+          <Box sx={{ display: { xs: 'none', md: 'flex' }, alignItems: 'center', mr: 2 }}>
+            <Link to="/" style={{ display: 'flex', alignItems: 'center', textDecoration: 'none', color: 'inherit' }}>
+              <img src={logo} alt="logo" style={{ height: 40, marginRight: 8 }} />
+              <Typography variant="h6" noWrap sx={{ fontWeight: 700, letterSpacing: '.1rem' }}>
+                Product Catalog
+              </Typography>
+            </Link>
+          </Box>
+
+          
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="open navigation"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{ display: { xs: 'block', md: 'none' } }}
+            >
+              {pages.map((page) => (
+                <MenuItem
+                  key={page.label}
+                  component={Link}
+                  to={page.path}
+                  onClick={handleCloseNavMenu}
+                >
+                  <Typography textAlign="center">{page.label}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+
+          
+          <Box sx={{ display: { xs: 'flex', md: 'none' }, alignItems: 'center', flexGrow: 1 }}>
+            <Link to="/" style={{ display: 'flex', alignItems: 'center', textDecoration: 'none', color: 'inherit' }}>
+              <img src={logo} alt="logo" style={{ height: 36, marginRight: 8 }} />
+              <Typography variant="h6" noWrap sx={{ fontWeight: 700, letterSpacing: '.1rem' }}>
+                Product Catalog
+              </Typography>
+            </Link>
+          </Box>
+
+         
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' }, justifyContent: 'flex-end' }}>
+            {pages.map((page) => (
+              <Button
+                key={page.label}
+                component={Link}
+                to={page.path}
+                onClick={handleCloseNavMenu}
+                sx={{
+                  my: 2,
+                  color: '#222',
+                  display: 'block',
+                  textTransform: 'none',
+                  fontWeight: 500,
+                  fontSize: '1rem'
+                }}
+              >
+                {page.label}
+              </Button>
+            ))}
+          </Box>
+
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
 }
 
-export default Navbar
+export default Navbar;
